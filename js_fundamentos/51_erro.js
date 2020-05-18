5577660c5978b56a1c762b0c69c2cca4efd24bf7
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-05-18 20:50:37 -0300

    Aula 50 à 52

---
+++ js_fundamentos/51_erro.js
@@ -0,0 +1,21 @@
+function tratarErroLancar(erro) {
+    // throw new Error('...')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroLancar(e)
+    } finally {
+        console.log('final')
+    }
+}
