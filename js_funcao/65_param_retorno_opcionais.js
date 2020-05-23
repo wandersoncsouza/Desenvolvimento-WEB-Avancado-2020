21b1d06bad476abff3932cd4e846e71505babf70
wandersoncsouza <53883005+wandersoncsouza@users.noreply.github.com>
2020-05-23 19:03:52 -0300

    Aula 65 a 69

---
+++ js_funcao/65_param_retorno_opcionais.js
@@ -0,0 +1,11 @@
+function area(largura, altura){
+    const area = largura * altura
+    if(area > 20){
+        console.log(`valor acima do permitido: ${area} m2.`)
+    } else{
+        return area
+    }
+}
+console.log(area(2,2))
+console.log(area(2))
+console.log(area(5,6))
